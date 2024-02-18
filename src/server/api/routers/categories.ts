76a0473e45d7b5c0029e7dff9fa08979b5be382e
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2024-02-18 14:52:40 -0600

    feat(api): add quote categories route to api

---
+++ src/server/api/routers/categories.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { categories } from "~/server/db/schema";
+ 
+export const categoryRouter = createTRPCRouter({
+  create: publicProcedure
+    .input(z.object({ 
+      name: z.string().min(1),
+      userId: z.number(),
+    }))
+    .mutation(async ({ ctx, input }) => {
+      await ctx.db.insert(categories).values({
+        name: input.name,
+        userId: input.userId,
+      });
+    }),
+});
