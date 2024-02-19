0cc4cb22476810ed54af8d2ce6993f0e65f32e45
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2024-02-19 08:51:21 -0600

    feat(api): add collections route to api and fix categories

--- src/server/api/routers/collections.ts
+++ src/server/api/routers/collections.ts
@@ -1,16 +1,18 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
-import { categories } from "~/server/db/schema";
+import { collections } from "~/server/db/schema";
  
-export const categorieRouter = createTRPCRouter({
+export const collectionRouter = createTRPCRouter({
   create: publicProcedure
     .input(z.object({ 
       name: z.string().min(1),
+      userId: z.number(),
     }))
     .mutation(async ({ ctx, input }) => {
-      await ctx.db.insert(categories).values({
+      await ctx.db.insert(collections).values({
         name: input.name,
+        userId: input.userId,
       });
     }),
 });
