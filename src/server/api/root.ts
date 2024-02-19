b093aba8a4e1dceb41540406ae7d8736c2454155
Moses Maximino Cosme II <mosesmaxcosme@gmail.com>
2024-02-19 08:54:34 -0600

    fix(api): add routes to app router and fix typos

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,10 +1,14 @@
+import { createTRPCRouter } from "~/server/api/trpc";
+
+// import routes
 import { userRouter } from "~/server/api/routers/users";
 import { parishRouter } from "~/server/api/routers/parishes";
 import { saintRouter } from "~/server/api/routers/saints";
 import { workRouter } from "~/server/api/routers/works";
 import { citationRouter } from "~/server/api/routers/citations";
+import { categoryRouter } from "~/server/api/routers/categories";
+import { collectionRouter } from "~/server/api/routers/collections";
 import { quoteRouter } from "~/server/api/routers/quotes";
-import { createTRPCRouter } from "~/server/api/trpc";
 
 /**
  * This is the primary router for your server.
@@ -18,6 +22,8 @@ export const appRouter = createTRPCRouter({
   work: workRouter,
   citation: citationRouter,
   quote: quoteRouter,
+  category: categoryRouter,
+  collection: collectionRouter,
 });
 
 // export type definition of API
