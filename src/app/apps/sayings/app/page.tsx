27cb66294065c2e89dbe15c8602924bd59355f6d
Moses Cosme <mosesmaxcosme@gmail.com>
2024-03-15 13:28:38 -0500

    feat(sayings): add quote of the day card to sayings home page

--- src/app/apps/sayings/app/page.tsx
+++ src/app/apps/sayings/app/page.tsx
@@ -1,6 +1,16 @@
 import { unstable_noStore as noStore } from "next/cache";
+import Image from "next/image";
 import Link from "next/link";
+
 import { Button } from "~/components/ui/button";
+import {
+  Card,
+  CardHeader,
+  CardFooter,
+  CardTitle,
+  CardDescription,
+  CardContent,
+} from "~/components/ui/card";
 
 export default async function SayingsApp() {
   noStore();
@@ -14,26 +24,39 @@ export default async function SayingsApp() {
           <span className="text-secondary-red-600">Sayings</span> of the Fathers
         </h1>
 
-        <p className="">
-          Quote of the Day: <br />
-          &quot;In church I was listening to a reading from the Prophet Isaiah,
-          and at the words, “Wash you make you clean,” I reflected, “Maybe the
-          Mother of God sinned at one time or another, if only in thought.” And,
-          marvelous to relate, in unison with my prayer a voice sounded in my
-          heart, saying clearly, “The Mother of God never sinned even in
-          thought.” Thus did the Holy Spirit bear witness in my heart to her
-          purity. &quot;
-          <br />
-          --
-          <Link href="/apps/sayings/app/saints/saint" className="font-bold">
-            Saint Silouan the Athonite
-          </Link>
-          , as found in
-          <Link href="/apps/sayings/app/works/work" className="italic">
-            St. Silouan the Athonite
-          </Link>
-          , pp. 391-2.
-        </p>
+        <Card>
+          <CardHeader className="text-lg">Quote of the Day</CardHeader>
+
+          <CardContent className="flex flex-row items-center justify-around gap-8 text-xl">
+            <Image
+              src={"/images/saints/St-Silouan-Athonite.jpg"}
+              alt="St Silouan The Athonite"
+              width={140}
+              height={200}
+            />
+
+            <p>
+              &quot;In church I was listening to a reading from the Prophet
+              Isaiah, and at the words, “Wash you make you clean,” I reflected,
+              “Maybe the Mother of God sinned at one time or another, if only in
+              thought.” And, marvelous to relate, in unison with my prayer a
+              voice sounded in my heart, saying clearly, “The Mother of God
+              never sinned even in thought.” Thus did the Holy Spirit bear
+              witness in my heart to her purity. &quot;
+              <br />
+              --
+              <Link href="/apps/sayings/app/saints/saint" className="font-bold">
+                Saint Silouan the Athonite
+              </Link>
+              , as found in
+              <Link href="/apps/sayings/app/works/work" className="italic">
+                St. Silouan the Athonite
+              </Link>
+              , pp. 391-2.
+            </p>
+          </CardContent>
+        </Card>
+
         <p>Search Form Goes Here</p>
         <p>Link to open advanced search modal</p>
 
