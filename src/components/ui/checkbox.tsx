33893135f9272fb5740104ebefe2072cfbf948b3
Moses Cosme <mosesmaxcosme@gmail.com>
2024-03-16 14:20:49 -0500

    feat(ui): add shadcn/ui checkbox

---
+++ src/components/ui/checkbox.tsx
@@ -0,0 +1,37 @@
+"use client";
+
+import * as React from "react";
+import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
+import Image from "next/image";
+
+import { cn } from "~/lib/utils";
+
+const Checkbox = React.forwardRef<
+  React.ElementRef<typeof CheckboxPrimitive.Root>,
+  React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>
+>(({ className, ...props }, ref) => (
+  <CheckboxPrimitive.Root
+    ref={ref}
+    className={cn(
+      "border-primary shadow focus-visible:ring-ring data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground peer h-4 w-4 shrink-0 rounded-sm border focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
+      className,
+    )}
+    {...props}
+  >
+    <CheckboxPrimitive.Indicator
+      className={cn("text-current flex items-center justify-center")}
+    >
+      <Image
+        src={`/images/icons/Check-Icon.svg`}
+        alt={"Check"}
+        height={16}
+        width={16}
+        aria-hidden="true"
+        className="h-4 w-4"
+      />
+    </CheckboxPrimitive.Indicator>
+  </CheckboxPrimitive.Root>
+));
+Checkbox.displayName = CheckboxPrimitive.Root.displayName;
+
+export { Checkbox };
