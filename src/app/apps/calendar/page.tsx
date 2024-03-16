508fcf7bd43c24b5c3fc559e75a5ee9c6af994b8
Moses Cosme <mosesmaxcosme@gmail.com>
2024-03-16 16:52:59 -0500

    fix(apps): fix app layout width

--- src/app/apps/calendar/page.tsx
+++ src/app/apps/calendar/page.tsx
@@ -5,7 +5,7 @@ export default async function CalendarPage() {
   noStore();
 
   return (
-    <main className="flex w-4/5 flex-col items-center justify-center text-neutral-900">
+    <main className="flex w-full flex-col items-center justify-center text-neutral-900">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1
           className={`font-synaxisHeader text-5xl font-extrabold tracking-tight text-primary-gold-600 sm:text-[5rem]`}
