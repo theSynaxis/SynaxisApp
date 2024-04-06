ea016d53cab10d730fd643749b6f14b4430f8f56
mosesintech <mosesmaxcosme@gmail.com>
2024-04-06 01:33:36 -0500

    feat(lives): add lives app routing

---
+++ src/app/apps/lives/page.tsx
@@ -0,0 +1,41 @@
+import { unstable_noStore as noStore } from "next/cache";
+import Link from "next/link";
+
+export default async function CalendarPage() {
+  noStore();
+
+  return (
+    <main className="flex w-full flex-col items-center justify-center text-neutral-900">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <h1
+          className={`font-synaxisHeader text-5xl font-extrabold tracking-tight text-primary-gold-600 sm:text-[5rem]`}
+        >
+          <span className="text-secondary-red-600">Lives</span> of the Saints
+        </h1>
+
+        <p className="w-1/2">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem saepe
+          corporis, quaerat veritatis ipsam, iure sapiente quibusdam assumenda
+          non, consequuntur quas accusamus? Suscipit inventore architecto illum
+          maiores nostrum reprehenderit laboriosam!
+        </p>
+
+        <div>
+          <h3>Download</h3>
+          <p>From Apple App Store</p>
+          <p>From Google Play Store</p>
+        </div>
+
+        <Link href="/apps/calendar/app">
+          <div>
+            <h3>Use in the Browser</h3>
+            <p>
+              It&rsquo;s also a browser app! You can use it right here in your
+              favorite browser.
+            </p>
+          </div>
+        </Link>
+      </div>
+    </main>
+  );
+}
