120bd68751079307f1d50314c11286b500e82510
mosesintech <mosesmaxcosme@gmail.com>
2024-05-09 20:45:23 -0500

    feat(apps): design apps landing page

--- src/app/apps/page.tsx
+++ src/app/apps/page.tsx
@@ -6,39 +6,72 @@ export default async function AppsPage() {
 
   return (
     <main className="flex w-full flex-col items-center justify-center text-neutral-900">
-      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+      <div className="container flex flex-col items-center justify-start gap-8 px-4 py-4">
         <h1
           className={`font-synaxisHeader text-5xl font-extrabold tracking-tight text-primary-gold-600 sm:text-[5rem]`}
         >
           The <span className="text-secondary-red-600">Synaxis</span> App
         </h1>
+        <h2>Apps for Eastern Orthodox Christians</h2>
 
-        <p className="w-1/2">
-          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Autem saepe
-          corporis, quaerat veritatis ipsam, iure sapiente quibusdam assumenda
-          non, consequuntur quas accusamus? Suscipit inventore architecto illum
-          maiores nostrum reprehenderit laboriosam!
-        </p>
-
-        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-3 md:gap-8">
           <Link
-            className="bg-white/10 hover:bg-white/20 flex max-w-xs flex-col gap-4 rounded-xl border border-neutral-900 p-4 shadow-md hover:shadow-lg"
-            href="/apps/sayings"
+            className="flex max-w-xs flex-col gap-4 rounded-xl border border-neutral-900 bg-white/10 p-4 shadow-md hover:bg-white/20 hover:shadow-lg"
+            href="/apps/lives"
           >
+            <h3 className="text-2xl font-bold">Lives →</h3>
+            <div className="text-lg">
+              Learn about the lives of holy men and women.
+            </div>
+          </Link>
+          <div className="flex max-w-xs flex-col gap-4 rounded-xl border border-neutral-900 bg-white/10 p-4 shadow-md">
+            <h3 className="text-2xl font-bold">Sayings - Coming Soon</h3>
+            <div className="text-lg">
+              Hear wisdom and truth from the friends of God.
+            </div>
+          </div>
+          <div className="flex max-w-xs flex-col gap-4 rounded-xl border border-neutral-900 bg-white/10 p-4 shadow-md">
+            <h3 className="text-2xl font-bold">Calendar - Coming Soon</h3>
+            <div className="text-lg">
+              Find the divine services in parishes near you.
+            </div>
+          </div>
+          <div className="flex max-w-xs cursor-default flex-col gap-4 rounded-xl border border-neutral-900 bg-white/10 p-4 text-neutral-50 shadow-md">
             <h3 className="text-2xl font-bold">Sayings →</h3>
             <div className="text-lg">
               Hear wisdom and truth from the friends of God.
             </div>
-          </Link>
-          <Link
-            className="bg-white/10 hover:bg-white/20 flex max-w-xs flex-col gap-4 rounded-xl border border-neutral-900 p-4 shadow-md hover:shadow-lg"
-            href="/apps/calendar"
-          >
-            <h3 className="text-2xl font-bold">Calendar →</h3>
+          </div>
+          <div className="flex max-w-xs cursor-default flex-col gap-4 rounded-xl border border-neutral-900 bg-white/10 p-4 text-neutral-50 shadow-md">
+            <h3 className="text-2xl font-bold">Sayings →</h3>
             <div className="text-lg">
-              Find the divine services in parishes near you.
+              Hear wisdom and truth from the friends of God.
             </div>
-          </Link>
+          </div>
+          <div className="flex max-w-xs cursor-default flex-col gap-4 rounded-xl border border-neutral-900 bg-white/10 p-4 text-neutral-50 shadow-md">
+            <h3 className="text-2xl font-bold">Sayings →</h3>
+            <div className="text-lg">
+              Hear wisdom and truth from the friends of God.
+            </div>
+          </div>
+          <div className="flex max-w-xs cursor-default flex-col gap-4 rounded-xl border border-neutral-900 bg-white/10 p-4 text-neutral-50 shadow-md">
+            <h3 className="text-2xl font-bold">Sayings →</h3>
+            <div className="text-lg">
+              Hear wisdom and truth from the friends of God.
+            </div>
+          </div>
+          <div className="flex max-w-xs cursor-default flex-col gap-4 rounded-xl border border-neutral-900 bg-white/10 p-4 text-neutral-50 shadow-md">
+            <h3 className="text-2xl font-bold">Sayings →</h3>
+            <div className="text-lg">
+              Hear wisdom and truth from the friends of God.
+            </div>
+          </div>
+          <div className="flex max-w-xs cursor-default flex-col gap-4 rounded-xl border border-neutral-900 bg-white/10 p-4 text-neutral-50 shadow-md">
+            <h3 className="text-2xl font-bold">Sayings →</h3>
+            <div className="text-lg">
+              Hear wisdom and truth from the friends of God.
+            </div>
+          </div>
         </div>
       </div>
     </main>
