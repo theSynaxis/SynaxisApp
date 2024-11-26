b881e6279beb6c61eb090dc84e9500774606d30d
mosesintech <mosesmaxcosme@gmail.com>
2024-11-25 19:04:54 -0600

    chore(cron): add todo note for future cron job: unban users

--- src/server/api/cron.ts
+++ src/server/api/cron.ts
@@ -1,5 +1,10 @@
-import { lucia } from './auth';
+import { lucia } from "./auth";
 
 export const destroySessions = async () => {
   await lucia.deleteExpiredSessions();
-}
+};
+
+// TODO: add cron job to unban users at specified ban times.
+// grab all banned users.
+// forEach user, compare bannedUntil time to new Date()
+// if day/month/year match, update user to isBanned = false.
